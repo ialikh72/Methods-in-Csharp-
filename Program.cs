8ff2f8f2ad598cc20726d83ccc12aa23fe34daca
ialikh72 <ialikh.w1@gmail.com>
2026-01-19 23:32:42 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Sumof2Numnbers( ) // function defination
+    {
+        Console.WriteLine("enter a:");
+        int a =int.Parse(Console.ReadLine());
+        Console.WriteLine("enter b:");
+        int b = int.Parse(Console.ReadLine());
+        int c = a + b;
+        Console.WriteLine("sum is:"+c);
+
+    }
+    static void Main()
+    {
+     Sumof2Numnbers(); // function call
+    }
+}
